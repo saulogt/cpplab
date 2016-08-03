0f848a430602f27070133cde29636737a05cd983
Saulo Tauil <saulogt@gmail.com>
2016-08-02 17:39:39 -0700

    Initial Commit

---
+++ cpplab/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpplab
+//
+//  Created by Saulo Tauil on 2016-08-02.
+//  Copyright © 2016 Saulo Tauil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
